f2e5bef020275c4091842510a9cafa4dff7c568b
github username <github email address>
2018-10-30 16:20:19 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    erb :welcome
   end
-end
+
+  #gives all posts
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  #directs to new posting submisson
+  get '/posts/new' do
+    erb :new
+  end
+
+  #show one post
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  #create posts
+  post '/posts' do
+    @post = Post.create(params[:post])
+    # binding.pry
+    
+    redirect :'/posts'
+  end
+  
+  #editing post
+  get '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by(id: params[:id])
+    @post.delete
+    redirect :"/posts"
+  end
+  
+end #end of app class
